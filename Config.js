65486b6bcd695ac2c28212ffdb3c28fd3bbb65f4
Pradumm2003 <pradummpatel739@gmail.com>
2025-05-14 21:18:32 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -19,11 +19,11 @@ global.prefa = process.env.PREFIX ? process.env.PREFIX.split(',') : ['', '!', '.
 global.autoRecording = process.env.AUTO_RECORDING === 'false';
 global.autoTyping = process.env.AUTO_TYPING === 'true';
 global.autorecordtype = process.env.AUTO_RECORD_TYPE === 'true';
-global.autoread = process.env.AUTO_READ === 'true';
+global.autoread = process.env.AUTO_READ === 'false';
 global.autobio = process.env.AUTO_BIO !== 'false'; // default true
 global.anti92 = process.env.ANTI_92 === 'true';
 global.autoswview = process.env.AUTO_SW_VIEW !== 'false'; // default true
-global.welcome = process.env.WELCOME !== 'true'; // default true
+global.welcome = process.env.WELCOME !== 'false'; // default true
 global.autoreact = process.env.AUTO_REACT === 'true';
 
 // Thumbnail profile picture
